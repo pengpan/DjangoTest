9428d05f10096c82adc53a3faf0d23aff6c54fc3
pengpan <i@hvv.me>
2019-07-28 19:39:30 +0800

    update

--- hello/urls.py
+++ hello/urls.py
@@ -3,4 +3,5 @@ from hello import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("json-data", views.json_data)
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,17 @@
+import json
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
 
 # Create your views here.
 
 def home(request):
     # return HttpResponse("Hello, Django!")
     return render(request, "index/index.html")
+
+
+def json_data(request):
+    result = {"name": "Not Sure", "age": 25, "city": "深圳"}
+    # return JsonResponse(result)
+    return HttpResponse(json.dumps(result, ensure_ascii=False), content_type="application/json,charset=utf-8")
