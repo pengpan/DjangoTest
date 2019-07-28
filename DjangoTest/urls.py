944995562196341d9ae96fa4f6468f710d14463c
pengpan <i@hvv.me>
2019-07-28 19:02:09 +0800

    update

--- DjangoTest/urls.py
+++ DjangoTest/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url='static/img/favicon.ico')),
     path('admin/', admin.site.urls),
     path("", include("hello.urls")),
 ]
--- hello/views.py
+++ hello/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    # return HttpResponse("Hello, Django!")
+    return render(request, "index/index.html")
