6d3ab609554e0c961d701b08ddb3b00c14242716
pengpan <i@hvv.me>
2019-07-28 19:06:09 +0800

    update

--- DjangoTest/urls.py
+++ DjangoTest/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.views import serve
 from django.urls import include, path
-from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('favicon.ico', RedirectView.as_view(url='static/img/favicon.ico')),
+    path('favicon.ico', serve, {'path': 'img/favicon.ico'}),
     path('admin/', admin.site.urls),
     path("", include("hello.urls")),
 ]
